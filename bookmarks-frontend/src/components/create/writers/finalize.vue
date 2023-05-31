48a6385d6d6d588c1cd7a9d2f0b1c6d65c5ac2ef
Mykalgitswaves <michaelfinal.png@gmail.com>
2023-05-31 15:43:42 -0700

    Addmore cowbell

--- bookmarks-frontend/src/components/create/writers/finalize.vue
+++ bookmarks-frontend/src/components/create/writers/finalize.vue
@@ -1,5 +1,27 @@
-<template>last one.</template>
+<template>
+    <div class="container grid place-content-center">
+        <RouterLink to="/home">
+            <button 
+                class="mt-5 px-36 py-4 bg-indigo-600 rounded-sm text-indigo-100"
+                type="button"
+            >Continue
+            </button>
+        </RouterLink>
+    </div>
+</template>
 
 <script>
-export default {}
+    import { useStore } from '../../../stores/page.js';
+
+export default {
+    methods: {
+        navigate() {
+            this.state.getNextPage()
+        },
+       
+    },
+    mounted() {
+        this.state = useStore()     
+    }
+}
 </script>
