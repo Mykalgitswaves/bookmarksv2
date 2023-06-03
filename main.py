ccbb4a21daed165f267f56058fbb0b25f7fa46e1
Mykalgitswaves <michaelfinal.png@gmail.com>
2023-06-02 18:42:57 -0700

    Import classes from db_helpers.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from typing import ClassVar, Optional, List
+from database.db_helpers import (
+    User,
+    Review,
+    Book,
+    Author,
+    Genre,
+    Tag,
+    Neo4jDriver
+)
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel, ClassList
 
 
 """
@@ -13,13 +20,3 @@ app = FastAPI()
 async def get_data():
     data = {"cashmoney": "cashmoeny"}
     return data
-
-@app.post("/users/{user}")
-async def create_user(user: UserRequest, params):
-    user = params
-    user = UserRequest(fullname=str(request.body.fullname), name=str(request.body.name))
-    return user
-
-# user1 = User(fullname="Cash", name="cash")
-
-# user1.merge()
