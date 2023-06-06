4879d3a68623e6354cba13abe5f47a512526e558
Mykalgitswaves <michaelfinal.png@gmail.com>
2023-06-05 22:02:51 -0700

    Add new create user form to add post to later.

--- bookmarks-frontend/src/views/createuser.vue
+++ bookmarks-frontend/src/views/createuser.vue
@@ -2,10 +2,9 @@
   <div class="h-100 grid place-content-center relative mt-10">
     <div class="flex flex-row gap-5 w-100 justify-center my-5">
       <button type="button" @click="getPrevPage">Prev</button>
-      <button 
-        class="text-indigo-500 mx-2 hover:text-indigo-800 underline underline-offset-2" 
-        type="button" 
-      >{{ page }}</button>
+      <p 
+        class="text-indigo-500 mx-2" 
+      ><span class="text-indigo-800 underline underline-offset-2">{{ page + 1}}</span> / 4</p>
       <button type="button" @click="getNextPage">Next</button>
     </div> 
     <component :is="createFormState" />
@@ -14,15 +13,18 @@
 
 <script>
 // Import create user form components
+import CreateUser from '@/components/create/createuserstart.vue'
 import CreateUserFormBooks from '@/components/create/createuserformbooks.vue'
 import CreateUserFormGenre from '@/components/create/createusergenre.vue'
 import CreateUserFormFinal from '@/components/create/createuserfinal.vue'
+
 
 import { computed } from 'vue'
 import { useStore } from '../stores/page.js'
 
 // Map to components keep the view the same
 const userFormMapping = {
+  0: CreateUser,
   1: CreateUserFormBooks,
   2: CreateUserFormGenre,
   3: CreateUserFormFinal
