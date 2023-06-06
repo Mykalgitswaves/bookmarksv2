97745ea246824071bcdf4e9f052010fb27b353ea
Mykalgitswaves <michaelfinal.png@gmail.com>
2023-06-05 17:30:27 -0700

    I need to touch grass tbh, taking a break

--- bookmarks-frontend/src/components/create/createusergenre.vue
+++ bookmarks-frontend/src/components/create/createusergenre.vue
@@ -1,46 +1,47 @@
 <template>
+<div class="max-w-[600px] mx-5">
   <h2 class="text-3xl font-medium mb-4">What are some of your favorite genres?</h2>
   <p class="text-gray-500">Tap on your favorite genres</p>
 
   <input
-    class="py-2 px-4 rounded-md border-2 border-indigo-200 mt-5 w-62 max-w-[600px]"
+    class="py-2 px-4 rounded-md border-2 border-indigo-200 mt-5 w-[100%]"
     @change="searchGenres($event)"
     placeholder="Search for genres"
     name="searchForBooks"
     type="text"
   />
 
-  <div class="container mt-20 mb-36">
-    <ul class="grid-pills justify-content-center">
-      <li
+  <div v-if="genres !== null">
+    <div class="w-60 container mt-5 mb-5 grid-pills justify-content-center">
+      <div
         @click="addGenre(genre)"
         v-for="genre in genres"
         :key="genre"
         class="bg-gray-100 px-2 text-center py-2 rounded-lg border-2 border-solid border-gray-300 hover:border-indigo-500 text-lg"
       >
-        <span>{{ genre.name }}</span>
-      </li>
-    </ul>
+        <span v-if="genres.detail !== 'Not found'">{{ genre.name }}</span>
+    </div>
   </div>
-  <div class="container mt-20 mb-36">
-    <ul class="grid-pills justify-content-center">
-      <li
-        v-for="genre in addedGenre"
-        :key="genre"
-        class="bg-gray-100 px-2 text-center py-2 rounded-lg border-2 border-solid border-gray-300 hover:border-indigo-500 text-lg"
-      >
-        <span>{{ genre.name }}</span>
-      </li>
-    </ul>
+    <div class="container mb-5 border-t-[1px] border-solid border-indigo-300">
+      <ul class="grid-pills justify-content-center">
+        <li
+          v-for="genre in addedGenre"
+          :key="genre"
+          class="bg-indigo-100 px-2 text-center py-2 rounded-lg border-2 border-solid border-indigo-300 hover:border-indigo-500 text-lg"
+        >
+          <span>{{ genre.name }}</span>
+        </li>
+      </ul>
+    </div>
   </div>
-
   <button
-    class="px-36 py-3 bg-indigo-600 rounded-md text-indigo-100"
+    class="block mt-5 px-20 py-3 bg-indigo-600 rounded-md text-indigo-100 w-[100%]"
     type="submit"
     @click="navigate"
   >
     Continue
   </button>
+</div>
 </template>
 
 <script>
@@ -51,7 +52,7 @@ import { toRaw } from 'vue';
 export default {
   data() {
     return {
-      genres: ['English', 'Romance', 'Mystery', 'Horror', 'Cooking', 'Graphic Novels'],
+      genres: null,
       addedGenre: [],
       state: null
     }
@@ -71,8 +72,10 @@ export default {
       try {
         const response = await fetch(`http://127.0.0.1:8000/genres/${event.target.value}`);
         const data = await response.json()
-        this.genres = data
-        console.log(data)
+        if(data !== "Not found") {
+          this.genres = data
+          console.log(data)
+        }
       } catch(err) {
         console.log(err)
       }
