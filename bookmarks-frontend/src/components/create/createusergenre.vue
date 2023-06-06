f9fd021bb7547b1accdf4cd4675b73b0262894e5
Mykalgitswaves <michaelfinal.png@gmail.com>
2023-06-05 19:00:52 -0700

    Fix small bugs with styling.

--- bookmarks-frontend/src/components/create/createusergenre.vue
+++ bookmarks-frontend/src/components/create/createusergenre.vue
@@ -1,10 +1,11 @@
 <template>
-<div class="max-w-[600px] mx-5">
+<div class="max-w-[600px] px-5">
   <h2 class="text-3xl font-medium mb-4">What are some of your favorite genres?</h2>
   <p class="text-gray-500">Tap on your favorite genres</p>
 
   <input
     class="py-2 px-4 rounded-md border-2 border-indigo-200 mt-5 w-[100%]"
+    v-bind="isEmpty"
     @change="searchGenres($event)"
     placeholder="Search for genres"
     name="searchForBooks"
@@ -17,9 +18,9 @@
         @click="addGenre(genre)"
         v-for="genre in genres"
         :key="genre"
-        class="bg-gray-100 px-2 text-center py-2 rounded-lg border-2 border-solid border-gray-300 hover:border-indigo-500 text-lg"
+        :class="isEmpty === false ? 'bg-gray-100 px-2 text-center py-2 rounded-lg border-2 border-solid border-gray-300 hover:border-indigo-500 text-lg' : ''"
       >
-        <span v-if="genres.detail !== 'Not found'">{{ genre.name }}</span>
+        {{ genre.name }}
     </div>
   </div>
     <div class="container mb-5 border-t-[1px] border-solid border-indigo-300">
@@ -54,7 +55,18 @@ export default {
     return {
       genres: null,
       addedGenre: [],
-      state: null
+      state: null,
+      isEmpty: ''
+    }
+  },
+  watch: {
+    genres: {
+      handler(newValue) {
+        console.log(newValue)
+        if (!newValue.length) {
+          this.isEmpty = true
+        } 
+      }
     }
   },
   methods: {
@@ -72,12 +84,14 @@ export default {
       try {
         const response = await fetch(`http://127.0.0.1:8000/genres/${event.target.value}`);
         const data = await response.json()
-        if(data !== "Not found") {
+        if(data.detail !== 'Not found') {
           this.genres = data
+          this.isEmpty = false
           console.log(data)
         }
       } catch(err) {
         console.log(err)
+        this.isEmpty = true
       }
     }
   },
