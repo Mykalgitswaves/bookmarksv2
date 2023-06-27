81bc998a36958aa1cfc867213c58b3b82b7532a0
Mykalgitswaves <michaelfinal.png@gmail.com>
2023-06-26 22:53:54 -0700

    add a new footer component and start building out dummy html and styles for app

--- bookmarks-frontend/src/components/feed/footernav.vue
+++ bookmarks-frontend/src/components/feed/footernav.vue
@@ -1,12 +1,14 @@
 <template>
-    <footer class="bg-gray-100">
-        
+    <footer>
+        <button class="footer-nav-button">
+            <IconBook/>
+        </button>
     </footer>
 </template>
 
 
 <script setup>
-
+import IconBook from '@/components/svg/icon-book.vue'
 
 </script>
 
@@ -19,5 +21,23 @@ footer {
  display: flex;
  justify-content: space-between;
  padding: 1.25rem;
+ background: rgba(243, 244, 246, 98%);
+}
+
+.footer-nav-button {
+    border-radius: 50%;
+    padding: .5rem;
+    margin-left: 1rem;
+    margin-right: 1rem;
+    color: #667EEA;
+    border: solid 2px #667EEA;
+    align-content: center;
+    justify-content: center;
+    transition-duration: 250ms;
+}
+.footer-nav-button:hover {
+    border: solid 2px #5A67D8;
+    color: #5A67D8;
+    transform: scale(1.02);
 }
 </style>
