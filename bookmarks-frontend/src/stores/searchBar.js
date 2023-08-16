f9cded6c9f6451d1a97155b3d6b083878441a21b
Mykalgitswaves <michaelfinal.png@gmail.com>
2023-08-15 18:13:39 -0700

    NerdShtiWithKYle.

---
+++ bookmarks-frontend/src/stores/searchBar.js
@@ -0,0 +1,18 @@
+import { defineStore } from 'pinia';
+import {ref} from 'vue'
+
+export const useStore = defineStore('searchResults' , () => {
+    
+        const data = ref(null)
+        function saveAndLoadSearchResults(newData){
+            // Check to see if there is data in storage or if ref is already not null
+            if(this.data === null){
+                // if not save new data store to data arg being passed in
+               this.data = newData;
+            } else {
+                // otherwise set this data = to search data saved in local storage
+                return this.data
+            }
+        }
+    return { data, saveAndLoadSearchResults }
+})
