28f50e1a0538adc57aa80e3fb9e1963e011f5cd3
Mykalgitswaves <michaelfinal.png@gmail.com>
2023-11-07 22:03:16 -0800

    works nerdage.

--- bookmarks-frontend/src/components/feed/WorkPage.vue
+++ bookmarks-frontend/src/components/feed/WorkPage.vue
@@ -1,92 +1,33 @@
 <template>
-    <section class="works-page-wrapper-margin">
-    <TransitionGroup name="work-page">
-            <button type="button" class="px-3 py-2 bg-indigo-500 text-white rounded-md" @click="backToFeed">Back</button>
-            <div :class="'works-wrapper shadow-lg mt-5 ' + activeWork">
-                <div class="">
-                    <img :src="image" :alt="title + 'image'" v-if="loaded">
-
-                    <div class="placeholder" v-if="!loaded"></div> 
-
-                    <p class="text-xl font-semibold mb-5">
-                        <span v-if="loaded">{{ title }}</span>
-                        <span>{{ fallbackTitle }}</span>
-                    </p>
-
-                    <p v-if="loaded" v-html="description"></p>
-                    <p v-if="!loaded">{{ fallbackDescription }}</p>
-                </div>
-
-                <div class="mt-5 container flex justify-between">
-                    <button 
-                        type="button"
-                        class="text-indigo-500 font-medium cursor-pointer"
-                        @click="isActiveWork = !isActiveWork"
-                    >
-                        Add to bookshelf
-                    </button>
-                    <button type="button"
-                        class="text-indigo-500 font-medium cursor-pointer"
-                    >
-                        Show Reviews
-                    </button>
-                </div>
-            </div>
-
-            <SimilarBooks/>
-
-            <p class="text-xl font-semibold text-slate-700 my-5">Reviews of this work</p>
-    </TransitionGroup>
+    <section>
+        what up doofas
     </section>
 </template>
 
 <script setup>
-import SimilarBooks from '@/components/feed/SimilarBooks.vue';
-import {ref, computed, onMounted } from 'vue';
-import { RouterLink, useRoute, useRouter } from 'vue-router'
+// import SimilarBooks from '@/components/feed/SimilarBooks.vue';
+import { ref } from 'vue';
+import { useRoute, useRouter } from 'vue-router'
+import { db } from '../../services/db';
+import { urls } from '../../services/urls';
 
 const router = useRouter();
 const route = useRoute();
+
 const book_id = route.params.work;
-const data = ref(null); 
-const isActiveWork = ref(false);
+const book = ref(null);
 
 function backToFeed() {
     return router.push(`/feed/${route.params.user}/all`)
 };
 
 async function getWorkPage() {
-    
-    console.log(route.params);
-    await fetch(`http://127.0.0.1:8000/api/books/${book_id}`)
-    .then((data) => data.json())
-    .then((res) => {
-        data.value = res.data
-    });
+    await db.get(urls.reviews.getBookPage(book_id), null, true);
 }
-
 getWorkPage()
 
-const description = computed(() => (data.value.description));
-const title = computed(() => (data.value.title));
-const image = computed(() => (data.value.img_url));
-const loaded = computed(() => data.value === null ? false : true);
-const activeWork = computed(() => isActiveWork.value ? 'bg-indigo-200': 'bg-slate-50')
-
-const fallbackTitle = 'Patience is a virtue';
-
-onMounted(() => {
-    console.log(router.currentRoute.value.path)
-})
 </script>
-
 <style scoped>
-
-    
-    .works-page-wrapper-margin {
-        margin-left: 1.5rem;
-        margin-right: 1.5rem;
-    }
     .works-wrapper {
         min-height: 250px;
         border-radius: .3rem;
--- bookmarks-frontend/src/services/urls.js
+++ bookmarks-frontend/src/services/urls.js
@@ -18,15 +18,23 @@ export const urls = {
         review: baseUrl + 'api/review/create_review',
         update: baseUrl + 'api/review/create_update',
         comparison: baseUrl + 'api/review/create_comparison',
+        createComment: () => (baseUrl + 'api/review/create_comment'),
+        // used in feed
         getReviews: (user_id) => (baseUrl +`api/${user_id}/posts`),
         getComparisons: (user_id) => (baseUrl + `api/${user_id}/comparisons`),
+        // getting post data including initial comments on click of comment btn
         getPost: (user_id, id) => (baseUrl + `api/${user_id}/posts/${id}/post`),
         likeComparison: (comparison_id, user_id) => (baseUrl + `api/${user_id}/like/comparisons/${comparison_id}`),
-        createComment: () => (baseUrl + 'api/review/create_comment'),
         deleteComment: (comment_id) => (baseUrl + `api/review/${comment_id}/delete`),
         likeComment: (comment_id) => (baseUrl + `api/review/${comment_id}/like`),
         unlikeComment: (comment_id) => (baseUrl + `api/review/${comment_id}/remove_like`),
         getComments: (post_id) =>(baseUrl + `api/review/${post_id}/comments`),
+        // calling more comments (duh)
         getMoreComments: (comment_id) => (baseUrl + `api/review/comments/${comment_id}/replies`),
+    },
+    books: {
+        // for book page
+        getBookPage: (book_id) => (baseUrl + `api/books/${book_id}`)
+        
     }
 }
