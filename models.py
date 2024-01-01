0af732aba18e033e79f64ace67f04c3188254437
Mykalgitswaves <michaelfinal.png@gmail.com>
2024-01-01 18:20:58 -0500

    new shit alert!

--- models.py
+++ models.py
@@ -53,6 +53,67 @@ class DoublyLinkedList:
         while n.next is not None:
             n = n.next
         n.prev.next = None
+    def reorder_node(self, node_data, prev_node_data, next_node_data):
+        if self.start_node is None:
+            print("The list is empty")
+            return
+
+        # Find the node to be moved
+        current = self.start_node
+        while current and current.item != node_data:
+            current = current.next
+
+        if not current:
+            print("Node to reorder not found")
+            return
+
+        # Detach the node from its current position
+        if current.prev:
+            current.prev.next = current.next
+        else:
+            self.start_node = current.next
+
+        if current.next:
+            current.next.prev = current.prev
+
+        # Find the new previous and next nodes
+        prev_node = None
+        next_node = self.start_node
+
+        if prev_node_data is not None:
+            prev_node = self.start_node
+            while prev_node and prev_node.item != prev_node_data:
+                prev_node = prev_node.next
+
+            if not prev_node:
+                print("Previous node not found")
+                return
+
+            next_node = prev_node.next
+
+        if next_node_data is not None:
+            next_node = self.start_node
+            while next_node and next_node.item != next_node_data:
+                next_node = next_node.next
+
+            if not next_node:
+                print("Next node not found")
+                return
+
+            prev_node = next_node.prev
+
+        # Place the node at the new position
+        current.prev = prev_node
+        current.next = next_node
+
+        if prev_node:
+            prev_node.next = current
+        else:
+            self.start_node = current
+
+        if next_node:
+            next_node.prev = current
+
     # Traversing and Displaying each element of the list
     def Display(self):
         if self.start_node is None:
@@ -64,3 +125,5 @@ class DoublyLinkedList:
                 print("Element is: ", n.item)
                 n = n.next
         print("\n")
+
+    
