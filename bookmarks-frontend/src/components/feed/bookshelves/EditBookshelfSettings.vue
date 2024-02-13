f47bf8ac2a7a78606ca27633973ed6ffca9fb3a2
Mykalgitswaves <michaelfinal.png@gmail.com>
2024-02-12 22:34:14 -0800

    Fe work on edit / create bookshelves.

--- bookmarks-frontend/src/components/feed/bookshelves/EditBookshelfSettings.vue
+++ bookmarks-frontend/src/components/feed/bookshelves/EditBookshelfSettings.vue
@@ -1,8 +1,56 @@
 <template>
-    <section>
-        Yoooo
+    <section class="settings-section mt-10">
+            <div class="settings-info-form">
+                <FormInputCluster 
+                    input-id="bookshelfTitle" 
+                    name="Title" 
+                    type="text"    
+                    :value="data.shelfTitle"
+                    @new-value-saved="($event) => updateFieldName($event, 'bookshelfTitle')"
+                />
+
+                <p class="text-sm text-slate-600">Description</p>
+                <textarea 
+                    name="description" id="" cols="30" rows="3"
+                    placeholder=""
+                    :disabled="isEditing"
+                    v-model="data.shelfDescription">
+                </textarea>
+                <button 
+                    type="button"
+                    class="save-btn mt-5"
+                    @click="updateFieldName(userData.bio, 'bookshelfDescription')"
+                >
+                    save
+                </button>
+            </div>
     </section>    
+    <div class="mobile-menu-spacer sm:hidden"></div>
 </template>
 <script setup>
+import FormInputCluster from '../settings/FormInputCluster.vue';
+import { reactive } from 'vue';
 
+const data = reactive({
+    loaded: false,
+    shelfTitle: 'Untitled',
+    shelfDescription: 'Add a description',
+    // TODO double check that designs dont have any other shit in them.
+});
+
+const urlsMapping = {
+    // TODO make endpoints for bookshelf pages.
+    // "username": urls.user.updateUsername,
+    // "email": urls.user.updateEmail,
+    // "bio": urls.user.updateBio
+}
+
+function updateFieldName(newDataToSave, keyForMapping) {
+    // TODO Make this into an external forms.js service function accepting urls mapping as an option.
+    // Need this to say whether or not email save is disabled
+    data[keyForMapping] = newDataToSave;
+    db.put(urlsMapping[keyForMapping](route.params.user), newDataToSave, true).then((res) => {
+        data[keyForMapping] = res.data;
+    });
+}
 </script>
