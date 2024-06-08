68e9d8a532e2b04e225036064008a4c0ffbb4754
Kyle <kyle.arbide@gmail.com>
2024-06-08 09:12:56 -0400

    deploying fronted through CDN

--- infrastructure/s3/frond_end_static_files/main.tf
+++ infrastructure/s3/frond_end_static_files/main.tf
@@ -23,3 +23,68 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.front_end.id
   policy = file("iam_policy.json")
 }
+
+resource "aws_s3_bucket_website_configuration" "website_config" {
+  bucket = aws_s3_bucket.front_end.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  # error_document {
+  #   key = "error.html"
+  # }
+}
+
+resource "aws_cloudfront_origin_access_identity" "my_oai" {
+  comment = "My OAI for S3 bucket"
+}
+
+resource "aws_cloudfront_distribution" "my_distribution" {
+  origin {
+    domain_name = "${aws_s3_bucket.front_end.bucket_regional_domain_name}"
+    origin_id   = "book-prod-front-end-static-files-origin"
+
+    s3_origin_config {
+      origin_access_identity =  aws_cloudfront_origin_access_identity.my_oai.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "book-prod-front-end-static-files-origin"
+
+    forwarded_values {
+      query_string = true   
+
+      cookies {
+        forward = "all"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+output "cloudfront_url" {
+  value = aws_cloudfront_distribution.my_distribution.domain_name
+}
