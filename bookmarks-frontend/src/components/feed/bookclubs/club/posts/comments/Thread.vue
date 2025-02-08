9781f09c7e25181320a4346e93915747bdf6e193
mykal myb <mykalmyb@mykals-MacBook-Pro.local>
2025-02-08 14:19:14 -0800

    threads

---
+++ bookmarks-frontend/src/components/feed/bookclubs/club/posts/comments/Thread.vue
@@ -0,0 +1,15 @@
+<template>
+    yo world
+</template>
+<script setup lang="ts">
+interface Thread {
+    
+}
+
+const props = defineProps({
+    thread: {
+        type: Object,
+        required: false, // false for now.
+    }
+});
+</script>
